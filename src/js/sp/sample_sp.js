c1a45b7daacb38f6f992965dc1d447678f7347e4
Sungjoon Park <sungjoon512@gmail.com>
2020-05-21 22:12:16 +0900

    feat: Add vue files

--- src/js/sp/sample_sp.js
+++ src/js/sp/sample_sp.js
@@ -1,4 +1,10 @@
+import Vue from 'vue';
 import '../../scss/sp/sample_sp.scss';
+import App from '../../vue/sample/App.vue';
+
+new Vue({
+  render: (h) => h(App),
+}).$mount('#app');
 
 // eslint-disable-next-line no-console
 document.body.insertAdjacentHTML('beforeend', 'sample sp text from JS file');
---
+++ src/vue/sample/App.vue
@@ -0,0 +1,22 @@
+<template>
+  <div>
+    <h2 :class="$style.heading">{{ text }} from vue file</h2>
+  </div>
+</template>
+
+<script>
+export default {
+  data() {
+    return {
+      text: 'Sample',
+    };
+  },
+};
+</script>
+
+<style lang="scss" module>
+.heading {
+  color: cadetblue;
+  font-weight: bold;
+}
+</style>
