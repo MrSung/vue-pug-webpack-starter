d0fb79550c2dc2578dd5e3e3587025e054541af3
Sungjoon Park <sungjoon512@gmail.com>
2020-05-21 21:40:42 +0900

    feat: Add multiple files incl. PC and SP directory

---
+++ src/js/pc/sample_pc.js
@@ -0,0 +1,4 @@
+import '../../scss/pc/sample_pc.scss';
+
+// eslint-disable-next-line no-console
+document.body.insertAdjacentHTML('beforeend', 'sample pc text from JS file');
---
+++ src/js/sp/sample_sp.js
@@ -0,0 +1,4 @@
+import '../../scss/sp/sample_sp.scss';
+
+// eslint-disable-next-line no-console
+document.body.insertAdjacentHTML('beforeend', 'sample sp text from JS file');
